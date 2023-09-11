e3a1a6e9eca6f3d4e591a454147f6244cf6b7a03
José Messias <josecmessias22@gmail.com>
2023-09-11 20:56:17 -0300

    subindo o front-end

---
+++ front-end/login/js/script.js
@@ -0,0 +1,37 @@
+const form = document.getElementById("form");
+const campos = document.querySelectorAll(".required");
+const spans = document.querySelectorAll(".span-required");
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+form.addEventListener('submit', (event) => {
+   event.preventDefault();
+   emailValidate();
+   senhaValidate();
+});
+
+function setError(index) {
+  campos[index].style.border = "2px solid #e63636";
+  spans[index].style.display = "block";
+}
+
+function removeError(index) {
+  campos[index].style.border = "";
+  spans[index].style.display = "none";
+}
+
+function emailValidate() {
+  if (!emailRegex.test(campos[0].value)) {
+    setError(0);
+  } else {
+    removeError(0);
+  }
+}
+
+function senhaValidate() {
+  if (campos[1].value.length < 8) {
+    setError(1);
+  } else {
+    removeError(1);
+    compareSenha();
+  }
+}
