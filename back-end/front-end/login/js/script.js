ed32722434b83080146714b070ed2cee27d049a0
JoseCMessias <josecmessias22@gmail.com>
2023-09-15 20:01:07 -0300

    update

--- back-end/front-end/login/js/script.js
+++ back-end/front-end/login/js/script.js
@@ -3,35 +3,66 @@ const campos = document.querySelectorAll(".required");
 const spans = document.querySelectorAll(".span-required");
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-form.addEventListener('submit', (event) => {
-   event.preventDefault();
-   emailValidate();
-   senhaValidate();
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    emailValidate();
+    senhaValidate();
+
+    const Email = document.getElementById("email").value;
+    const Senha = document.getElementById("senha").value;
+
+    const login_Data = { Email, Senha };
+
+    try {
+        const resposta = await fetch("/auth/login", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+
+            body: JSON.stringify(login_Data),
+        });
+
+        const data = await resposta.json();
+
+        if (resposta.ok) {
+            console.log(data.msg);
+
+            localStorage.setItem("jwtToken", data.token);
+
+            const user_id = data.id;
+
+            window.location.href = `/Home?id=${user_id}`;
+        } else {
+            console.log(data.msg);
+        }
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 function setError(index) {
-  campos[index].style.border = "2px solid #e63636";
-  spans[index].style.display = "block";
+    campos[index].style.border = "2px solid #e63636";
+    spans[index].style.display = "block";
 }
 
 function removeError(index) {
-  campos[index].style.border = "";
-  spans[index].style.display = "none";
+    campos[index].style.border = "";
+    spans[index].style.display = "none";
 }
 
 function emailValidate() {
-  if (!emailRegex.test(campos[0].value)) {
-    setError(0);
-  } else {
-    removeError(0);
-  }
+    if (!emailRegex.test(campos[0].value)) {
+        setError(0);
+    } else {
+        removeError(0);
+    }
 }
 
 function senhaValidate() {
-  if (campos[1].value.length < 8) {
-    setError(1);
-  } else {
-    removeError(1);
-    compareSenha();
-  }
+    if (campos[1].value.length < 8) {
+        setError(1);
+    } else {
+        removeError(1);
+    }
 }
--- back-end/script.js
+++ back-end/script.js
@@ -1,67 +1,110 @@
 // Imports
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-const app = express()
+const app = express();
 
 // Config JSON
-app.use(express.json())
+app.use(express.json());
 
 // Models
-const usuario = require('./models/usuario')
+const usuario = require("./models/usuario");
 
 // Rota aberta - Rota pública - Página principal
-app.use(express.static('front-end'))
-app.get('/', (req, res) => {
-    res.sendFile(__dirname+'/front-end/Cadastro/cadastro.html')
-})
+app.use(express.static("front-end"));
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/front-end/login/login.html");
+});
 
 // registrar usuário
-app.post('/auth/cadastro', async(req, res) =>{
-    const {Nome,Email,Senha,Conf_senha} = req.body
+app.post("/auth/cadastro", async (req, res) => {
+    const { Nome, Email, Senha, Conf_senha } = req.body;
 
     const userExiste = await usuario.findOne({
-        Email: Email
-    })
+        Email: Email,
+    });
 
-    if(userExiste){
+    if (userExiste) {
         return res.status(422).json({
-            msg: 'Email já está cadastrado'
-        })
+            msg: "Email já está cadastrado",
+        });
     }
 
-    const salt = await bcrypt.genSalt(10)
-    const SenhaHash = await bcrypt.hash(Senha, salt)
+    const salt = await bcrypt.genSalt(10);
+    const SenhaHash = await bcrypt.hash(Senha, salt);
 
     const Usuario = new usuario({
-        Nome, 
+        Nome,
         Email,
         Senha: SenhaHash,
-    })
+    });
 
-    try{
-        await Usuario.save()
+    try {
+        await Usuario.save();
         res.status(201).json({
-            msg: "Usuário criado com sucesso!"
-        })
-    }
-    catch(err){
-        console.log(err)
+            msg: "Usuário criado com sucesso!",
+        });
+    } catch (err) {
+        console.log(err);
 
-        res.status(500).json(error)
+        res.status(500).json(error);
     }
+});
+
+// login
+
+app.get("/auth/login", async (req, res) =>{
+    const {Email, Senha} = req.body
+
+        if(!Email){
+            return res.status(422).json({msg: 'Email obrigatório'})
+        }
+
+        if(!Senha){
+            return res.status(422).json({msg: 'Senha obrigatório'})
+        }
+
+        const user = await usuario.findOne({Email: Email})
+        
+        if(!Email){
+            return res.status(404).json({msg: 'Usuário não encontrado'})
+        }
+
+        const valida_Senha = await bcrypt.compare(Senha, user.Senha) 
+
+        if(!valida_Senha){
+            return res.status(422).json({mgs: 'Senha inválida'})
+        }
+
+        try {
+            const Segredo = process.env.SEGREDO
+
+            const token = jwt.sign({
+                id: user._id
+            }, 
+                Segredo
+            );
+
+            res.status(200).json({msg: 'Login realizado com sucesso!', id: user._id, token})
+
+        } catch (error) {
+            console.log(err)
+        }
+        
 })
 
-// credenciais 
+// credenciais
 
 mongoose
-.connect(`mongodb+srv://dmthysgo:0123@cluster0.cr4w0re.mongodb.net/DataBase?retryWrites=true&w=majority`)
-.then(() => {
-    app.listen(3000)
+    .connect(
+        `mongodb+srv://messiasdb:vuda-b8f9@apiproject.rkwieor.mongodb.net/messiasdb?retryWrites=true&w=majority`
+    )
+    .then(() => {
+        app.listen(3000);
 
-    console.log('Conectado com o mongoDB')
-})
-.catch((err) => console.log(err))
+        console.log("Conectado com o mongoDB");
+    })
+    .catch((err) => console.log(err));
